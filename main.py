eb46bf5058502616bcb56a58182c91ae168cdd6a
Axilleas Ismaili <74646111+ismaAXILLEAS@users.noreply.github.com>
2020-11-24 03:45:09 +0300

    Reads input and product price

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 if __name__ == '__main__':
     """
     Ένα κατάστημα προσφέρει 10 άτοκες μηνιαίες δόσεις στα προϊόντα του. 
-    
-    Να γράψετε αλγόριθμος   το οποίο :
-    α) διαβάζει την τιμή ενός προϊόντος
     """
 
-    "    print message: type product price "
     print('Type product price: ')
 
+    productPrice = input()
 
-    """
-    productPrice=user input
-    print productPrice
-    """
+    print('The product price is ' + productPrice)
