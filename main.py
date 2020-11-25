9e17ce508e4516777a02cc0b2b3ee553a45e7cbe
Axilleas Ismaili <74646111+ismaAXILLEAS@users.noreply.github.com>
2020-11-26 01:36:09 +0300

    Calculate and print value added tax

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ if __name__ == '__main__':
     """
     Ένα κατάστημα προσφέρει 10 άτοκες μηνιαίες δόσεις στα προϊόντα του. 
     """
-
     print('Type product price: ')
-
     productPrice = input()
-
     print('The product price is ' + productPrice)
+
+    floatProductPrice = float(productPrice)
+    valueAddedTax = (24 / 100) * floatProductPrice
+    print('The value added tax is ' + str(valueAddedTax))
