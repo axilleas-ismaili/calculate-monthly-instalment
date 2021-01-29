1ff8ac3043a2ab0bd84d7258051bef701d6de93f
Axilleas Ismaili <74646111+ismaAXILLEAS@users.noreply.github.com>
2021-01-30 00:56:15 +0200

    Calculate value added tax.

---
+++ katas/008/main.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    product_price = input('Type the product price: ')
+    product_price = float(product_price)
+    value_added_tax = 24/100 * product_price
+    print(value_added_tax)
